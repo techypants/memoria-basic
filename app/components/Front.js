5da6b1f82e0d2ad7c5179bca8ce86a4f6a5bb38e
techypants <tanmaycool2102@gmail.com>
2024-06-08 18:15:23 +0530

    git added

--- app/components/Front.js
+++ app/components/Front.js
@@ -1,9 +1,9 @@
 import React from "react";
-
+import { IoLogoGithub } from "react-icons/io";
 export default function Front({ children }) {
   return (
     <div
-      className="flex items-center justify-around lg:h-[50%]"
+      className="flex items-center justify-around lg:h-[50%] pt-[40px] mb:[-60px] lg:mb-[60px] "
       style={{
         backgroundImage: "linear-gradient(to bottom,#FCDC94,#e4cc43)",
       }}
@@ -11,13 +11,19 @@ export default function Front({ children }) {
       <div className="w-[80%] flex lg:flex-row flex-col items-center justify-around">
         <span className="w-full items-center flex flex-col">
           <span className="text-[50px] lg:text-[100px] text-[#9b8030] font-bold">
-            Word-Memory{" "}
+            Memoria{" "}
           </span>
           <a
             href="https://tanmay-pant-portfolio.vercel.app/"
             className="m-auto mt-4 lg:mt-0 text-[20px] lg:text-[32px] p-2 border-[#9b8030] border-2 rounded-lg font-semibold hover:bg-[#a08430] hover:text-white text-[#a08430]"
           >
             Tanmay Pant
+          </a>
+          <a
+            href="https://github.com/techypants/memoria"
+            className="text-[32px] text-[#7a6524] mt-4"
+          >
+            <IoLogoGithub />
           </a>
           <div className="mt-8 text-md lg:text-xl text-[#594918] font-semibold">
             <ol>
